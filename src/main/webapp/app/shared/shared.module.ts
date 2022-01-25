4634d72cae46b309e22b9c753f47de1d1a2f2d05
Nestor <nestor.echegoyen@upcnecochea.com.ar>
2022-01-25 07:27:41 -0300

    Pantalla telefonista inicio

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -12,10 +12,12 @@ import { FormatMediumDatePipe } from './date/format-medium-date.pipe';
 import { SortByDirective } from './sort/sort-by.directive';
 import { SortDirective } from './sort/sort.directive';
 import { ItemCountComponent } from './pagination/item-count.component';
+import { FormatShortTimePipe } from './date/format-short-time.pipe';
 
 @NgModule({
   imports: [SharedLibsModule],
   declarations: [
+    FormatShortTimePipe,
     FindLanguageFromKeyPipe,
     TranslateDirective,
     AlertComponent,
@@ -41,6 +43,7 @@ import { ItemCountComponent } from './pagination/item-count.component';
     SortByDirective,
     SortDirective,
     ItemCountComponent,
+    FormatShortTimePipe,
   ],
 })
 export class SharedModule {}
